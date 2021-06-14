2715d3635ce5a7216081dfe071003a771debd9a6
vedant pol <polvedant7@gmail.com>
2021-06-14 23:32:43 +0530

    Finding Square Roots Problem Code: FSQRT
    In olden days finding square roots seemed to be difficult but nowadays it can be easily done using in-built functions available across many languages .
    
    Assume that you happen to hear the above words and you want to give a try in finding the square root of any given integer using in-built functions. So here's your chance.
    
    Input
    The first line of the input contains an integer T, the number of test cases. T lines follow. Each line contains an integer N whose square root needs to be computed.
    
    Output
    For each line of the input, output the square root of the input integer, rounded down to the nearest integer, in a new line.
    
    Constraints
    1<=T<=20
    1<=N<=10000
    Input:
    3
    10
    5
    10000
    
    Output:
    3
    2
    100

---
+++ Finding_Square_Roots.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{int a;
+cin>>a;
+while(a--){
+int x;
+ scanf("%d", &x);  
+ int restul;
+ restul=sqrt(x);
+ printf("%d\n", restul); 
+
+
+}
+return 0;
+}
